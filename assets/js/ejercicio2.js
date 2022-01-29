7820f86954beb50215812b25eaeb29b121b15320
Danilo Carrasco <danilo.carrasco.95@gmail.com>
2022-01-29 20:28:20 -0300

    Documentación del JS del ejercicio 2

--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,3 +1,4 @@
+// Cambia el color de la caja según el color del boton clickado
 let cambiarColorCaja = (boton) => {
 
     let colorBoton = boton.style.backgroundColor;
@@ -6,9 +7,9 @@ let cambiarColorCaja = (boton) => {
     boton.addEventListener("click", () => {
         caja.style.backgroundColor = colorBoton;
     });
-
 };
 
+// Declaración de los elementos de los botones
 let btn1 = document.getElementById("btn-1");
 let btn2 = document.getElementById("btn-2");
 let btn3 = document.getElementById("btn-3");
@@ -16,6 +17,7 @@ let btn4 = document.getElementById("btn-4");
 let btn5 = document.getElementById("btn-5");
 let btn6 = document.getElementById("btn-6");
 
+// Se ejecutan las escuchas de los eventos al clickar uno de los botones
 cambiarColorCaja(btn1);
 
 cambiarColorCaja(btn2);
